370f4f2b211e31957a864154a5977686320cbed1
Rodina ElFeky <rodina.elfeky228@gmail.com>
2026-05-19 20:14:22 +0300

    Database Connection and Server Updates

--- server/src/config/mongodb.js
+++ server/src/config/mongodb.js
@@ -1,13 +1,20 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const connectDB = async () => {
+  mongoose.connection.on("connected", () => {
+    console.log("MongoDB Connected");
+  });
 
-    mongoose.connection.on('connected', () => {
-       console.log("DB Connected")
-    })
+  mongoose.connection.on("error", (err) => {
+    console.log("MongoDB error:", err);
+  });
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/Nexora`)
-
-}
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/Nexora`);
+  } catch (error) {
+    console.error("Connection failed:", error);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
--- server/src/server.js
+++ server/src/server.js
@@ -1,20 +1,9 @@
-import express from 'express'
-import cors from 'cors'
 import 'dotenv/config'
+import app from './app.js'
 import connectDB from './config/mongodb.js'
 
-const app = express()
 const port = process.env.PORT || 7000
 connectDB()
-
-
-app.use(express.json())
-app.use(cors())
-
-
-app.get('/', (req,res)=>{
-   res.send("API Working")
-})
 
 
 app.listen(port, ()=> console.log('Server started on PORT : ' + port))
